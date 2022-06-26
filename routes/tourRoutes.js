a3a527a12530020f16d19511d434c598e605cb76
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-06-26 22:43:13 +0300

    Modules + middleware + chaining multiple middleware (and param middleware)

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const fs = require('fs');
+const tourController = require('./../controllers/tourController')
 
 const router = express.Router(); // It is a real middleware which must be used for '/api/v1/tours' route
-
 //ROUTES
 
-    router
+router.param('id', tourController.checkID)
+
+//Create a checkBody middleware
+//Check if body contains name and price property
+//If not send 400 status code
+
+    
         .route('/')
         .get(tourController.getAllTours)
-        .post(tourController.createTour);
+        .post(tourController.checkBody , tourController.createTour);
 
     router
         .route('/:id')

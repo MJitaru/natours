ace75560d28e0967010694499bbb0faab516652b
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-06-27 00:14:18 +0300

    Environment variables update

--- app.js
+++ app.js
@@ -7,30 +7,28 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1)MIDDLEWARES
-//middleware 1:
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
-//middleware 2:
+
 app.use(express.json()); //middleware
+app.use(express.static(`${__dirname}/public`))
 
-//middleware 3:
 app.use((req, res, next) => {
     console.log('Hello from the middleware 🙋‍♀️');
     next();
 });
 
-//middleware 4:
 app.use((req,res,next)=>{
     req.requestTime = new Date().toISOString();
     next();
-});
+})
 
 // 4) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
-
 
 

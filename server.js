182d8a583b82c4e99fd59cbe5f90209704b59c94
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-06-28 22:42:12 +0300

    adding tour schema (new mongoose.Schema) with name, rating and price Save it to a variable.

--- server.js
+++ server.js
@@ -10,10 +10,25 @@ mongoose.connect(DB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
-}).then(con => {
-  console.log(con.connections);
-  console.log('DB connection successful!');
-})
+}).then(() => console.log('DB connection successful!'));
+
+//Adding implementation schema with mongoose
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  } ,
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 

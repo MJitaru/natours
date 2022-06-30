7fb8defb3b4ff3f856b459488e5e2d90054d6267
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-06-30 22:10:33 +0300

    filtering + advance filtering and sorting using query string

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,8 +7,41 @@ const Tour = require ('../models/tourModel')
 //ROUTE HANDLERS
 exports.getAllTours = async (req,res)=>{
     try{
-        const tours = await Tour.find();
+    console.log(req.query);
+
+    // BUILD QUERY
+
+    // 1A) Filtering 
+        // eslint-disable-next-line node/no-unsupported-features/es-syntax
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit','fields'];
+        excludedFields.forEach(el=>delete queryObj[el]);
+    
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
      
+    let query = Tour.find(JSON.parse(queryStr)); 
+
+    // 2) SORTING
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+        //sort('price ratingsAverage')
+    } else {
+        query = query.sort('-createdAt') // sort in a descending order based on the time they were created, so that the newest ones appear first.
+    }
+       
+    //EXECUTE QUERY
+        const tours = await query;
+
+    /* const query = Tour.find()
+        .where('duration')
+        .equals(5)
+        .where('difficulty')
+        .equals('easy')*/
+
+    //SEND RESPONSE     
          res.status(200).json({
              status: 'success',
              results: tours.length,

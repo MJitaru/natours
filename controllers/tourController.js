cbc76cf46e313bc8f5830e5e0cbd0ef55fbb0cf5
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-07-01 12:45:20 +0300

    Refactoring code. tourController => removed the  filter, sort, limitFields and paginate methods and added in another module. utils/apiFeatures => Creating APIFeatures as a separate module in utils - as an utility module- (where I add a constructor function which contains filter, sort, limitFields and paginate methods)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,61 +1,24 @@
 const Tour = require ('../models/tourModel')
+const APIFeatures = require('../utils/apiFeatures')
 
-    //const tours = JSON.parse(
-    //fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`) ;
- 
+//Middleware
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
 
 //ROUTE HANDLERS
 exports.getAllTours = async (req,res)=>{
     try{
-    console.log(req.query);
-
-    // BUILD QUERY
-
-    // 1A) Filtering 
-        // eslint-disable-next-line node/no-unsupported-features/es-syntax
-        const queryObj = {...req.query};
-        const excludedFields = ['page','sort','limit','fields'];
-        excludedFields.forEach(el=>delete queryObj[el]);
-    
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-     
-    let query = Tour.find(JSON.parse(queryStr)); 
-
-    // 2) SORTING
-    if(req.query.sort) {
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy);
-        //sort('price ratingsAverage')
-    } else {
-        query = query.sort('-createdAt _id') // sort in a descending order based on the time they were created and id, so that the newest ones appear first.
-    };
-
-    // 3) FIELD limiting
-
-    if(req.query.fields) {
-        const fields = req.query.fields.split(',').join(' ');
-        query = query.select(fields);
-    } else {
-        query = query.select('-__v')
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page-1) * limit;
-
-    //page=3&limit=10
-    query=query.skip(skip).limit(limit);
-
-    if(req.query.page) {
-        const numTours =  await Tour.countDocuments();
-        if(skip >= numTours) throw new Error ('This page does not exist');
-    }
-       
     //EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+        const tours = await features.query;
     
 
     //SEND RESPONSE     

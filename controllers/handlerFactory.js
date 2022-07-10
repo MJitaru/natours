799871e1b08a65d58942d0a7f2de80b6d6287892
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-07-10 15:53:50 +0300

    Ading factory functions for (get tour/user/review, get all users/reviews/tours)

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -48,3 +48,25 @@ exports.createOne = Model => catchAsync(async (req,res,next) => {
     }
   });
 });
+
+//popOptions = populate Options
+exports.getOne = (Model, popOptions) => catchAsync(async (req,res,next) => {
+  let query = Model.findById(req.params.id);
+  if(popOptions) query = query.populate(popOptions);
+  const doc = await query;
+
+  const doc = await Model.findById(req.params.id).populate('reviews');
+  // Tour.findOne({ _id: req.params.id })
+
+  if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc
+    }
+});
+
+});
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const reviewController = require('../controllers/reviewController');
+const authController = require ('../controllers/authController');
 
-const router = express.Router()
+const router = express.Router({ mergeParams: true }); //by default each router only has access to the params of their specific routes
+
+// POST  /tour/234fasd4/reviews 
+// GET  /tour/234fasd4/reviews 
+// POST /reviews
 
 router
         .route('/')
-        .get(reviewController.getAllReviews);
+        .get(reviewController.getAllReviews)
+        .post(
+                authController.protect,           //1. Before posting review, the user must be authenticated (logged in)
+                authController.restrictTo('user'),//2. After login, the review can only be added by the users (not possible for admin or tour-guides)
+                reviewController.setTourUserIds,
+                reviewController.createReview)    //3. After 1 and 2 are fulfilled, middleware with review creation is runned.
 
 router
-        .route('/reviews')
-        .get(reviewController.createReview);
+        .route('/:id')
+        .get(reviewController.getReview)
+        .patch(reviewController.updateReview)
+        .delete(reviewController.deleteReview);
 
 module.exports = router;

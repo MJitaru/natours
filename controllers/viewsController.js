e4af2d3b255fb2110de62008a15a4ada7b578bbe
Madalin Jitaru <jitaru_madalin@yahoo.com>
2022-07-12 20:33:02 +0300

    Creating mixins for pug files and add them into separate included pug files

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,34 @@
-exports.getOverview = (req,res)=>{
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-};
+const Tour = require ('../models/tourModel');
+const catchAsync = require ('../utils/catchAsync');
 
-exports.getTour = (req,res)=>{
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
+exports.getOverview = catchAsync(async(req, res, next)=>{
+    // 1) Get tour data from collection
+    const tours = await Tour.find();
+
+    // 2) Build template 
+    // 3) Render that template using the tour data from step 1
+
+
+
+    res.status(200).render('overview', {
+        title: 'All Tours',
+        tours
     });
-};
+});
+
+exports.getTour = catchAsync(async (req,res)=>{
+    // Get the data, for the requested tour (including reviews and guides)
+    const tour = await Tour.findOne({slug : req.params.slug}).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    })
+
+    // Build template
+
+    // 3) Render template using data from 1)
+
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour',
+        tour
+    });
+});
